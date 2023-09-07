998957f8d7ac8521759a89e1fd91dec5f9643603
katie ing <katieing.7@gmail.com>
2023-09-07 21:44:14 +0000

    "Add readme"

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -45,13 +45,13 @@ public class JavaGrepImp implements JavaGrep {
     @Override
     public void process() throws IOException {
         List<String> matchedLines = new ArrayList<>();
-        //Loop through files in root directory using listFiles() method
+        //Loop through files in root directory using listFiles()
         List<File> files = listFiles(rootPath);
         for (File file : files) {
             List<String> lines = readLines(file);
-            //Loop through lines in file using readLines() method
+            //Loop through lines in file using readLines()
             for (String line : lines) {
-                //Check if line contains pattern using containsPattern() method
+                //Check if line contains pattern using containsPattern()
                 if (containsPattern(line)) {
                     matchedLines.add(line);
                 }
